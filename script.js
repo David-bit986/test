efbbafc52e721bb36ee1fe8dcede806dc90b2844
David-bit986 <75855764+David-bit986@users.noreply.github.com>
2024-03-14 05:58:37 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,149 @@
+const versesText = document.querySelector(".versesText")
+let versesSplitArry = versesText.innerText.split(" ")
+const versesLength = versesText.innerText.split(" ").length
+
+
+function prepareText() {
+    versesText.innerText = ' ';
+    versesSplitArry.forEach((word, id) => {
+        const text = document.createTextNode(`${word} `);
+        const span = document.createElement("span");
+        if (word.includes('BR')) {
+            const br = document.createElement("br");
+            versesText.appendChild(br);
+        } else {
+            span.setAttribute('class', 'anotationColor');
+            versesText.appendChild(span)
+            span.appendChild(text)
+        }
+    });
+}
+
+
+function colorText() {
+    prepareText();
+    const anotationColor = document.querySelectorAll('.anotationColor');
+    const colors = {
+        bBlue: [11, 20, 21, 24, 26, 36, 41, 44, 52, 74, 87, 97, 105, 107, 108, 109, 110, 112, 121, 133, 136, 138, 143, 151],
+        blue: [142, 149, 159, 162, 173, 175],
+        red: [5, 19, 22, 170],
+        green: [47, 56, 78],
+        dGreen: [106, 115, 128, 137, 144, 146, 147, 160],
+        purple: [15, 23, 38, 127, 129, 145, 161],
+        dPurple: [150, 152, 155, 156, 164, 167, 168, 172, 174],
+        orange: [46, 55, 62, 66, 76, 77, 81, 82, 88, 89, 98, 99, 119, 120, 126, 135, 139, 141, 154, 158, 170, 176],
+        grey: [58, 104, 114],
+        dPink: [86, 92],
+        yello: [101, 90],
+        customValue: [6, 13, 32, 34, 63, 67, 131, 140, 179]
+    };
+
+    anotationColor.forEach((word, index) => {
+        setTimeout(() => {
+            Object.entries(colors).forEach(([color, indexes]) => {
+                if (indexes.includes(index)) {
+                    if (color === 'customValue') {
+                        switch (index) {
+                            case 6:
+                                word.style.background = "linear-gradient(90deg, rgba(208,1,207,1) 51%, rgba(45,244,253,1) 52%)";
+                                break;
+                            case 13:
+                                word.style.background = "linear-gradient(90deg, rgba(228,9,11,1) 46%, rgba(248,1,255,1) 46%, rgba(248,1,255,1) 70%, rgba(6,251,251,1) 70%)";
+                                break;
+                            case 32:
+                                word.style.background = "linear-gradient(90deg, rgba(6,251,251,1) 22%, rgba(253,0,3,1) 22%)";
+                                break;
+                            case 34:
+                                word.style.background = "linear-gradient(90deg, rgba(195,12,204,1) 60%, rgba(6,251,251,1) 60%)";
+                                break;
+                            case 63:
+                                word.style.background = "linear-gradient(90deg, rgba(155,153,155,1) 58%, rgba(6,251,251,1) 58%)";
+                                break;
+                            case 67:
+                                word.style.background = "linear-gradient(90deg, rgba(252,150,2,1) 41%, rgba(1,255,1,1) 41%)";
+                                break;
+                            case 131:
+                                word.style.background = "linear-gradient(90deg, rgba(252,150,2,1) 58%, rgba(0,235,234,1) 58%)";
+                                break;
+                            case 140:
+                                word.style.background = "linear-gradient(90deg, rgba(55,107,25,1) 48%, rgba(229,2,233,1) 48%)";
+                                break;
+                            case 179:
+                                word.style.background = "linear-gradient(90deg, rgba(255,0,3,1) 50%, rgba(246,146,0,1) 50%, rgba(246,146,0,1) 50%, rgba(246,146,0,1) 76%, rgba(255,255,255,0) 76%)";
+                                break;
+                        }
+                    } else {
+                        word.style.background = getColorStyle(color);
+                    }
+                }
+            });
+        }, index * 220);
+
+
+    });
+
+    setTimeout(() => {
+        colorText();
+    }, 50000);
+
+};
+
+function getColorStyle(color) {
+    switch (color) {
+        case 'bBlue':
+            return "rgb(0,253,255)";
+        case 'blue':
+            return "rgb(0,1,207)";
+        case 'red':
+            return "rgb(253,1,1)";
+        case 'green':
+            return "rgb(2,253,2)";
+        case 'purple':
+            return "rgb(208,1,207)";
+        case 'orange':
+            return "rgb(254,151,1)";
+        case 'dPurple':
+            return "rgb(129,0,210)";
+        case 'grey':
+            return "rgb(152,151,152)";
+        case 'dPink':
+            return "rgb(171,78,121)";
+        case 'yello':
+            return "rgb(255,254,3)";
+        case 'dGreen':
+            return "rgb(51,110,33)";
+        default:
+            return "";
+    };
+
+};
+
+
+colorText();
+
+
+
+function animateText() {
+    return new Promise((resolve) => {
+        let animationsCompleted = 0;
+        versesSplitArry.forEach((word) => {
+            if (word.id == '1') {
+                word.style.background = "blue";
+            } else {
+                spwordan.style.background = "red";
+            }
+            animationsCompleted++;
+            if (animationsCompleted === word.length) {
+                resolve();
+            }
+        })
+    })
+}
+
+
+
+
+
 
 document.addEventListener('DOMContentLoaded', function () {
     // Get the scroll-element
@@ -5,44 +151,34 @@ document.addEventListener('DOMContentLoaded', function () {
     var imageGap = document.querySelector('.imgGap');
     var albums = document.querySelector('.albumGrid');
     var disk = document.querySelector('.disk');
-    var divElement = document.querySelector(".gallery");
-    var photos = document.querySelectorAll(".border");
+    var proiectInfoHeight = document.querySelector('.proiectInfo').offsetHeight; // Height of the proiectInfo section
+
     // Listen for the scroll event
     window.addEventListener('scroll', function () {
         // Calculate the scale based on the scroll position
 
-        var scale = -100 + window.scrollY / 10; // Adjust the divisor to control the sensitivity
-        var size = (window.scrollY / 1000) * 2
+        var scale = -100 + (window.scrollY - proiectInfoHeight) / 10; // Adjusted the scroll position for the new section
+        var size = ((window.scrollY - proiectInfoHeight) / 1000) * 2; // Adjusted the scroll position for the new section
 
         // Apply the scale transformation
-
-        if (window.scrollY < 1000) {
-            scrollElement.style.transform = `translateX(${scale}vw)`;
-            if (window.innerWidth > 801) {
-                imageGap.style.margin = `0 ${-scale}vw`;
-                disk.style.transform = `translate(-50%, ${-60 + window.scrollY / 10}%)`;
-                if (scale < -25) {
-                    albums.style.transform = `translateY(-25vh) scale(${3 - size})`;
-                } else {
-                    albums.style.transform = `translateY(${scale}vh) scale(${3 - size})`;
+        if (window.scrollY > 930) {
+            if (window.scrollY < proiectInfoHeight + 1000) { // Adjusted the scroll position for the new section
+                scrollElement.style.transform = `translateX(${scale}vw)`;
+                if (window.innerWidth > 801) {
+                    imageGap.style.margin = `0 ${-scale}vw`;
+                    disk.style.transform = `translate(-50%, ${-60 + (window.scrollY - proiectInfoHeight) / 10}%)`;
+                    if (scale < -25) {
+                        albums.style.transform = `translateY(-25vh) scale(${3 - size})`;
+                    } else {
+                        albums.style.transform = `translateY(${scale}vh) scale(${3 - size})`;
+                    }
                 }
+            } else {
+                scrollElement.style.transform = `translateX(-2vw)`;
             }
-        } else {
-            scrollElement.style.transform = `translateX(-2vw)`;
         }
-
-
     });
 });
-
-
-
-
-
-
-
-
-
 
 
 
@@ -94,7 +230,7 @@ const cursor = {
         var self = this;
 
         // Anchor hovering
-        document.querySelectorAll('a').forEach(function (el) {
+        document.querySelectorAll('img').forEach(function (el) {
             el.addEventListener('mouseover', function () {
                 self.cursorEnlarged = true;
                 self.toggleCursorSize();
@@ -104,6 +240,17 @@ const cursor = {
                 self.toggleCursorSize();
             });
         });
+
+
+        document.querySelector('.titleGradient').addEventListener('mouseover', function () {
+            self.cursorEnlarged = true;
+            self.toggleCursorSize();
+        });
+        document.querySelector('.titleGradient').addEventListener('mouseout', function () {
+            self.cursorEnlarged = false;
+            self.toggleCursorSize();
+        });
+
 
         // Click events
         document.addEventListener('mousedown', function () {
